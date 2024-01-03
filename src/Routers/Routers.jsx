85eb84d63594a44b51f27873053386dce3b24014
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-01-03 20:12:11 +0600

    setup basic folder structer

---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import HomePage from "../Pages/HomePage/HomePage/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>,
+      },
+    ],
+  },
+]);
+export default router;
