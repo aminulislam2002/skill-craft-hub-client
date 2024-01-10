a41fcce5b8f7ee9bcd86e270c3b27669217f5ef2
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-01-10 20:59:43 +0600

    update footer

--- src/Pages/SharedPages/Footer/Footer.jsx
+++ src/Pages/SharedPages/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import { FaFacebook, FaTwitter, FaInstagram, FaGoogle, FaLinkedin, FaApple, FaGooglePlay } from "react-icons/fa";
 
 import sch_logo from "../../../assets/logo/sch-logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div>
-      <div className="px-8 bg-[#151515] text-white border-b border-b-gray-700">
+      <div className="px-4 md:px-6 lg:px-8 bg-[#151515] text-white border-b border-b-gray-700">
         <div className="container mx-auto pt-20 pb-32">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
             <div>
@@ -17,24 +18,48 @@ const Footer = () => {
             </div>
             <div>
               <h4 className="text-lg mb-8 uppercase">COMPANY</h4>
-              <p className="text-sm mb-3">About us</p>
-              <p className="text-sm mb-3">Contact</p>
-              <p className="text-sm mb-3">Blog</p>
-              <p className="text-sm mb-3">Become a teacher</p>
+              <p className="text-sm mb-3">
+                <Link>About us</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Contact</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Blog</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Become a teacher</Link>
+              </p>
             </div>
             <div>
               <h4 className="text-lg mb-8 uppercase">PROGRAMS</h4>
-              <p className="text-sm mb-3">Nanodegree Plus</p>
-              <p className="text-sm mb-3">Veterans</p>
-              <p className="text-sm mb-3">Georgia</p>
-              <p className="text-sm mb-3">Self-Driving Car</p>
+              <p className="text-sm mb-3">
+                <Link>Nanodegree Plus</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Veterans</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Georgia</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Self-Driving Car</Link>
+              </p>
             </div>
             <div>
               <h4 className="text-lg mb-8 uppercase">SUPPORT</h4>
-              <p className="text-sm mb-3">Documentation</p>
-              <p className="text-sm mb-3">Forums</p>
-              <p className="text-sm mb-3">Language Packs</p>
-              <p className="text-sm mb-3">Release Status</p>
+              <p className="text-sm mb-3">
+                <Link>Documentation</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Forums</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Language Packs</Link>
+              </p>
+              <p className="text-sm mb-3">
+                <Link>Release Status</Link>
+              </p>
             </div>
             <div>
               <h4 className="text-lg mb-8 uppercase">MOBILE</h4>
@@ -68,21 +93,31 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="px-8 bg-[#151515] text-white">
+      <div className="px-4 md:px-6 lg:px-8 bg-[#151515] text-white">
         <div className="container mx-auto pt-4 pb-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <div className="flex justify-start items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 lg:gap-4">
+            <div className="flex justify-center lg:justify-start items-center">
               <img src={sch_logo} alt="Logo of SCH" />
               <h1 className="text-2xl font-bold font-Nunito text-left ms-5">SCH</h1>
             </div>
 
             <div className="flex justify-center items-center">
-              <ul className="flex justify-center items-center gap-8">
-                <li>Home</li>
-                <li>Privacy</li>
-                <li>Terms</li>
-                <li> Sitemap</li>
-                <li>Purchase</li>
+              <ul className="flex justify-center items-center gap-4 md:gap-6 lg:gap-8">
+                <li>
+                  <Link to="">Home</Link>
+                </li>
+                <li>
+                  <Link to="">Privacy</Link>
+                </li>
+                <li>
+                  <Link to="">Terms</Link>
+                </li>
+                <li>
+                  <Link to="">Sitemap</Link>
+                </li>
+                <li>
+                  <Link to="">Purchase</Link>
+                </li>
               </ul>
             </div>
 
@@ -109,7 +144,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="bg-black text-white py-6">
+      <div className="bg-black text-white py-6 px-8">
         <p className="text-center">Copyright © 2023 SCH by Aminul Islam. All Rights Reserved.</p>
       </div>
     </div>
